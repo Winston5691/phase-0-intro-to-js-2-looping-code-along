97351a9c7df43480c8fc2f98d6b60e7aaaa2e345
Wiston <winston.langat@student.moringaschool.com>
2022-08-22 22:35:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(array, string){
+    const newArray = []
+    for(let i = 0 ; i < array.length; i++){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArray;
+}
+console.log(writeCards(['Ada', 'Brendan', 'Ali'], "surprise"));
+
+function countDown(number){
+let i = number;
+while (i > -1){
+        console.log(i--);
+    }}
+
+countDown(10);
